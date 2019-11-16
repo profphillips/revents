9aa975903d30effee42f388e888c664de8283f70
John Phillips <profphillips@gmail.com>
2019-11-16 09:33:48 -0600

    End of Section 3

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import { Grid, Button } from 'semantic-ui-react'
-import EventList from '../EventList/EventList'
-import EventForm from '../EventForm/EventForm'
+import React, { Component } from 'react';
+import { Grid, Button } from 'semantic-ui-react';
+import EventList from '../EventList/EventList';
+import EventForm from '../EventForm/EventForm';
 
 const eventsFromDashboard = [
   {
@@ -52,23 +52,38 @@ const eventsFromDashboard = [
       }
     ]
   }
-]
-
+];
 
 class EventDashboard extends Component {
-    render() {
-        return (
-            <Grid>
-                <Grid.Column width={10}>
-                    <EventList events={eventsFromDashboard} />
-                </Grid.Column>
-                <Grid.Column width={6}>
-                    <Button positive content='Create Event' />
-                    <EventForm />
-                </Grid.Column>
-            </Grid>
-        )
-    }
+  state = {
+    events: eventsFromDashboard,
+    isOpen: false
+  };
+
+  handleIsOpenToggle = () => {
+    this.setState(({isOpen}) => ({
+      isOpen: !isOpen
+    }))
+  }
+
+  render() {
+    const { events, isOpen } = this.state;
+    return (
+      <Grid>
+        <Grid.Column width={10}>
+          <EventList events={events} />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <Button
+            onClick={this.handleIsOpenToggle}
+            positive
+            content="Create Event"
+          />
+          {isOpen && <EventForm cancelFormOpen={this.handleIsOpenToggle} />}
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
 export default EventDashboard;
